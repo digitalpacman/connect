89794e712e57b5727e14533a1be788e04d268b9a
Joseph Godfrey <joseph.godfrey@shipstation.com>
2021-04-26 18:47:44 -0500

    include metadata in carrier app sample

---
+++ samples/carrier-api/src/definitions/carrier-2.ts
@@ -0,0 +1,73 @@
+import {
+  Carrier,
+  PackageAttribute,
+  ShippingService,
+  PackageType,
+} from '@shipengine/connect-carrier-api';
+
+
+const aPackage: PackageType = {
+  Id: "a8195f65-8381-4a0e-8857-67deaa92ac3b",
+  Name: "UPS Letter",
+  CarrierPackageTypeCode: "01",
+  Description: "Letter",
+  Abbreviation: "Ltr",
+  PackageAttributes: [
+    PackageAttribute.International,
+    PackageAttribute.Domestic,
+  ] ,
+  RequiredToShip: ["Weight"],
+};
+
+const aShippingService: ShippingService = {
+  Id: "beb82e53-a5bb-4da3-a22a-be22c4e4e3c7",
+  Name: "UPS Express®",
+  Abbreviation: "Express",
+  Code: "07",
+  International: false,
+  RequiredProperties: ["Dimensions", "Weight"],
+  SupportedLabelSizes: ["Inches4x6"],
+  SupportedCountries: [
+    {
+      FromCountry: "GB"
+    },
+    {
+      FromCountry: "AU"
+    }
+  ],
+  Class: "OneDayEarly",
+  Grade: "Expedited",
+  ServiceAttributes: ["Returns", "MultiPackage", "Tracking"]
+};
+
+export const CarrierTwo: Carrier = {
+  Id: '',
+  Name: 'The Second Carrier',
+  Description: 'Babys second carrier',
+  PackageTypes: [aPackage],
+  ShippingServices: [aShippingService],
+  ShippingOptions: [
+    {
+      Name: 'Contains Dry Ice',
+      Type: 'contains-dry-ice',
+    },
+    {
+      Name: 'Contains Alcohol',
+      Type: 'contains-alcohol',
+    }
+  ],
+  DefaultSupportedCountries: [
+    {
+      FromCountry: 'US',
+    },
+    {
+      FromCountry: "GB",
+    }
+  ],
+  DefaultLabelSizes: ['Inches4x6', 'Inches4x8'],
+  LabelFormats: ['PDF', 'ZPL'],
+  DefaultConfirmationTypes: null,
+  CarrierAttributes: null,
+  TrackingUrl: 'https://the.second-carrier.com/track',
+  CarrierUrl: 'https://the.second-carrier.com',
+};
--- samples/carrier-api/src/index.ts
+++ samples/carrier-api/src/index.ts
@@ -9,8 +9,11 @@ import {
   CancelPickup,
   Track
 } from './methods';
+import { Metadata } from './definitions';
 
 export default new CarrierApp({
+  Metadata,
+
   Register,
   GetRates,
   CreateLabel,
